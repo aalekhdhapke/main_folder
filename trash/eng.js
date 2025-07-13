740c8b70fb228c3244bb812156ea60025822a443
aalekhdhapke <165888236+aalekhdhapke@users.noreply.github.com>
2025-07-13 11:57:14 +0530

    Add files via upload

---
+++ trash/eng.js
@@ -0,0 +1,4 @@
+const english = {
+    heading:"DETA SCIENCE ",
+    paragraph:"Data science is an interdisciplinary field that uses scientific methods, processes, algorithms, and systems to extract knowledge and insights from data. It combines various fields like statistics, computer science, and domain expertise to analyze and interpret data, ultimately helping businesses make informed decisions. Data science is crucial for uncovering hidden patterns, building predictive models, and developing actionable strategies."
+    };
---
+++ trash/spanish.js
@@ -0,0 +1,4 @@
+const spanish = {
+    heading:"CIENCIA DE DATOS ",
+    paragraph:" La ciencia de datos es un campo interdisciplinario que utiliza métodos, procesos, algoritmos y sistemas científicos para extraer conocimiento e información de los datos. Combina diversas disciplinas como la estadística, la informática y la experiencia en el sector para analizar e interpretar datos, ayudando así a las empresas a tomar decisiones informadas. La ciencia de datos es crucial para descubrir patrones ocultos, crear modelos predictivos y desarrollar estrategias viables."
+    };
